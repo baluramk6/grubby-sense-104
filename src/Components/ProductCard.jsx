2cb91221805dce22d73ef5f49274c29eff665959
Arjun Hore <arjunhore2611@gmail.com>
2022-11-09 12:00:55 +0530

    Card Added

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProductCard = ({ item }) => {
+  return (
+    <div id={item.id}>
+      <img src={item.avatar} />
+      <p>{item.name}</p>
+      <p>{item.price}</p>
+      <div>
+        <button>ADD TO BASKET</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
