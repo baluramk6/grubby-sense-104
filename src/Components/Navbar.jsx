3abf2d8b03ceffe361f6869eeac54ce3b8501fad
Rohit Kumar <rajr54827@gmail.com>
2022-11-11 12:06:10 +0530

    nav_dropdown

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,20 +9,115 @@ export const Navbar=()=>{
                 <img src={logo} alt="" />
 
             </div>
-            <div className={styles.nav_links}>
-                <div>
-                    <NavLink to={"/product"}>Women</NavLink>
-                    <div className="hover_box">
-                     <div className="1">
-                      
+            <div className={`${styles.nav_links}` }>
+                <div className={styles.women_box}>
+                    <div className={styles.women_box}>
+
+                    <NavLink  className={({ isActive }) =>
+                  isActive ? styles.navlink_active:   styles.navlink_default
+                } to={"/product"}>Women</NavLink>
+
+                <div className={styles.hover_box}>
+                     <div className={styles.efWear}>
+                        <div> <NavLink><b>Ethnic & Fusion Wear</b></NavLink></div>
+                      div  <div><NavLink >Kurtas & Kurtis</NavLink></div>
+                        <div><NavLink >Kurta Sets</NavLink></div>
+                        <div> <NavLink>Ethnic Dresses</NavLink></div>
+                        <div><NavLink>Tops & Tunics</NavLink></div>
+                        <div><NavLink >Leggings & Churidars</NavLink></div>
+                        <div> <NavLink >Pants & Palazzos</NavLink></div>
+                        <div><NavLink >Skirts</NavLink></div>
+                       <div> <NavLink >Dupattas & Stoles</NavLink>
                      </div>
-                     
-                     <div className="2"></div>
-                     <div className="3"></div>
-                     <div className="4"></div>
-                     <div className="5"></div>
-                     <div className="6"></div>
                     </div>
+                     
+                     
+                    
+                     
+                     
+                    
+                     
+                    
+                     
+                <div className={styles.Western_Wear}>
+                     <div> <NavLink><b>Western Wear</b></NavLink></div>
+
+                        <div><NavLink >Tops & Tees</NavLink></div>
+
+                        <div><NavLink >Dresses & Jumpsuits</NavLink></div>
+
+                        <div><NavLink >Jeans & Jeggings</NavLink></div>
+
+                        <div><NavLink >Shorts & Skirts</NavLink></div>
+
+                        <div><NavLink >Trousers</NavLink></div>
+
+                        <div><NavLink>Jackets & Shrugs</NavLink></div>
+                     </div>
+
+                <div className={styles.Sleepwear}>
+                     <div> <NavLink><b>Lingerie/Sleepwear</b></NavLink></div>
+
+                    <div><NavLink>Bras</NavLink></div>
+
+                    <div><NavLink>Briefs/Panties</NavLink></div>
+
+                    <div><NavLink>Cami & Slips</NavLink></div>
+
+                    <div><NavLink>Shapewear</NavLink></div>
+
+                    <div><NavLink>Nightwear</NavLink></div>
+                </div>
+
+                 <div className={styles.SportsWear}>
+
+                     <div> <NavLink><b>SportsWear</b></NavLink></div>
+
+                    <div><NavLink>Tops & Tees</NavLink></div>
+
+                    <div><NavLink >Sports Bra</NavLink></div>
+
+                    <div><NavLink >Leggings</NavLink></div>
+
+                    <div><NavLink >Joggers & Tracks</NavLink></div>
+
+                    <div><NavLink >Sweatshirts & Jackets</NavLink></div>
+                </div>
+
+                <div className={styles.topbrands}>
+                     <div> <NavLink ><b>Top Brands</b></NavLink></div>
+
+                    <div><NavLink >Melange</NavLink></div>
+
+                    <div><NavLink >Ginger</NavLink></div>
+
+                    <div><NavLink >Code</NavLink></div>
+
+                    <div><NavLink>Fame Forever</NavLink></div>
+
+                    <div><NavLink >Kappa</NavLink></div>
+                    <div><NavLink >Biba</NavLink></div>
+
+                    <div><NavLink >Aurelia</NavLink></div>
+
+                    <div><NavLink >AND</NavLink></div>
+
+                    <div><NavLink >SAllen Solly</NavLink></div>
+                    
+                </div>
+
+                     <div className={styles.women_category}>
+                     <div> <NavLink ><b>Winter wear</b></NavLink></div>
+                     <div> <NavLink ><b>Beauty</b></NavLink></div>
+                     <div> <NavLink ><b>Footwear/Bags</b></NavLink></div>
+                     <div> <NavLink ><b>Watches</b></NavLink></div>
+                     <div> <NavLink ><b>Jewellery</b></NavLink></div>
+                     <div> <NavLink ><b>Fragrances</b></NavLink></div>
+                     </div>
+                  </div>
+                </div>
+                  
+                    
                 </div>
 
                 <div>
